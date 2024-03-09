7f74ae4b006a015dd53ff85f996d60e883e58fd6
Misha Malyshev <misha@playdex.io>
2024-03-09 11:24:05 +0300

    :sparkles: Add repro code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'path';
+import initCycleTLS from 'cycletls';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
+}
+
+async function testCycleTLS(log: (...s: any[]) => void) {
+  log('init cycletls...');
+  const cycleTLS = await initCycleTLS({
+    port: 65116,
+    timeout: 15000,
+    debug: true,
+  });
+  log('init ok');
+  log('request...');
+  const response = await cycleTLS(
+    `https://api.kanye.rest`,
+    {
+      body: '',
+      ja3: '771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0',
+      userAgent:
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0',
+    },
+    'get'
+  );
+
+  if (response.status === 200) {
+    log('request ok', response.body);
+  } else {
+    log('error', response.status);
+  }
 }
 
 const createWindow = () => {
@@ -20,11 +48,19 @@ const createWindow = () => {
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
   }
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    testCycleTLS((string) =>
+      mainWindow.webContents.send('console-log', string)
+    );
+  });
 };
 
 // This method will be called when Electron has finished
--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,8 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { ipcRenderer } from 'electron';
+
+ipcRenderer.on('console-log', (_event, arg) => {
+  console.log(arg);
+});
